487fd54a5f71911e4773f221a286f01e2ba488a9
tanvi153pedanekar <tanvipedanekardadina153@gmail.comgit congig --global user.name git config --global user.email you@example.com  git config --global user.name Your>
2021-10-17 10:22:41 +0800

    and file created

---
+++ and.py
@@ -0,0 +1,27 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot, save_model
+import pandas as pd
+import numpy as np
+
+AND = {
+    "x1":[0,0,1,1],
+    "x2":[0,1,0,1],
+    "y":[0,0,0,1]
+    }
+
+df_AND = pd.DataFrame(AND)
+
+
+X,y = prepare_data(df_AND)
+
+ETA = 0.3
+EPOCHS = 10
+
+
+model_AND = Perceptron(eta = ETA, epochs = EPOCHS)
+model_AND.fit(X, y)
+_ = model_AND.total_loss()
+
+save_model(model_AND,"and.model")
+
+save_plot( df_AND, "and.png", model_AND)
