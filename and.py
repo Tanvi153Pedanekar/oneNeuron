07ea10b24f25517c1dfac00974320be6125a0659
tanvi153pedanekar <tanvipedanekardadina153@gmail.comgit congig --global user.name git config --global user.email you@example.com  git config --global user.name Your>
2021-10-20 15:33:13 +0800

    updated

--- and.py
+++ and.py
@@ -3,29 +3,35 @@ from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 import numpy as np
 
-def main():
- AND = {
+def main(data, eta, epochs,filename,plotFileName):
+ 
+
+ df = pd.DataFrame(data)
+
+
+ X,y = prepare_data(df)
+
+
+
+
+ model = Perceptron(eta = eta, epochs = epochs)
+ model.fit(X, y)
+ _ = model.total_loss()
+
+ save_model(model,filename)
+
+ save_plot( df, plotFileName, model)
+
+if __name__ == "__main__":
+
+    AND = {
     "x1":[0,0,1,1],
     "x2":[0,1,0,1],
     "y":[0,0,0,1]
     }
 
- df_AND = pd.DataFrame(AND)
 
+    ETA = 0.3
+    EPOCHS = 10
 
- X,y = prepare_data(df_AND)
-
- ETA = 0.3
- EPOCHS = 10
-
-
- model_AND = Perceptron(eta = ETA, epochs = EPOCHS)
- model_AND.fit(X, y)
- _ = model_AND.total_loss()
-
- save_model(model_AND,"and.model")
-
- save_plot( df_AND, "and.png", model_AND)
-
-if __name__ == "__main__":
- main()
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and.model", plotFileName="and.png")
